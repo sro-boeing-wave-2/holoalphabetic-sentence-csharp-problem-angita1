3ee3a1c4f329fbe819b61d2b4a4113c09bb4ff12
Yaiphabee <ksh.yaipha@gmail.com>
2018-07-19 23:39:56 +0530

    Remove blank lines

--- HoloalphabeticSentence/HoloalphabeticSentence.cs
+++ HoloalphabeticSentence/HoloalphabeticSentence.cs
@@ -8,10 +8,7 @@ namespace HoloalphabeticSentence
         // Write the logic to determine whether the input string is a HoloalphabeticSentence or not
         public static bool IsHoloalphabeticSentence(string input, string alphabets = "abcdefghijklmnopqrstuvwxyz")
         {
-
             return alphabets.All(input.ToLower().Contains);
-
-
         }
     }
 }
